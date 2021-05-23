388a990e358fc64ca42ea6e3daf6b05b2e95bde1
krashr-ds <kylierrn@gmail.com>
2021-05-23 00:35:05 -0400

    HCA 203 County Level Data Updates

--- PUF_aggregator.py
+++ PUF_aggregator.py
@@ -5,6 +5,7 @@ import pandas as pd
 SSA2FIPS = utils.load_data("data", "ssa_fips_state_county2011_augmented.csv")
 BRFSS = utils.load_data("data", "BRFSSCountyAggregates2010.csv")
 PUF = utils.load_data("data/PUF", "PUF_COUNTY_SUMMARY.csv")
+PUF2 = utils.load_data("data/PUF", "PUF_COUNTY_SUMMARY_50.csv")
 RWJF = utils.load_data("data", "RWJF2010.csv")
 
 # Only Need CBSA Info from SSA2FIPS
@@ -22,12 +23,16 @@ RWJF["CountyFIPS"] = RWJF["FIPS"].replace(to_replace=match_fips, value=r"\2", re
 
 # Join CBSA Information into PUF Data
 PUF_FIPS = pd.merge(PUF, SSA2FIPS, on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], how='left')
+PUF2_FIPS = pd.merge(PUF2, SSA2FIPS, on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], how='left')
 
 # Join in RWJF Rankings
 PUF_RWJF = pd.merge(PUF_FIPS, RWJF, left_on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], right_on=["StateCode", "CountyFIPS"], how='left')
+PUF2_RWJF = pd.merge(PUF2_FIPS, RWJF, left_on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], right_on=["StateCode", "CountyFIPS"], how='left')
 
 # Finally, join in the BRFSS data for the 302 counties that have it.
 PUF_BRFSS = pd.merge(PUF_RWJF, BRFSS, left_on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], right_on=["StateCode", "CountyFIPS"], how='left')
+PUF2_BRFSS = pd.merge(PUF2_RWJF, BRFSS, left_on=["FIPS_STATE_CODE", "FIPS_COUNTY_CODE"], right_on=["StateCode", "CountyFIPS"], how='left')
 
 # Write to CSV
 PUF_BRFSS.to_csv("data/PUF/PUF_BRFSS_MERGED.csv")
+PUF2_BRFSS.to_csv("data/PUF/PUF_BRFSS_MERGED_50.csv")
